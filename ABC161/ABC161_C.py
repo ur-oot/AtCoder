71bd975a9278c24de318898e412132df65ff9903
ur_oot <c.gs.nov.26t@gmail.com>
2021-10-18 21:42:37 +0900

    ABC161C

--- ABC161/ABC161_C.py
+++ ABC161/ABC161_C.py
@@ -1,11 +1,5 @@
 N, K = map(int, input().split())
 
-x = N % K
+ans = min(K - (N % K), N % K)
 
-while True:
-    if x > abs(x - K):
-        x = abs(x - K)
-    else:
-        break
-
-print(x)
+print(ans)
